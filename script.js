4f649af5ec22805f0b5a6b6396974182f1a06139
MABEL QUISPE OLIVERA <131488004+mabelolivera10@users.noreply.github.com>
2024-05-14 09:37:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+ 
+
+  $('.valentines-day').click(function(){
+      // Animación de desvanecimiento de los elementos del sobre
+      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+      $('.envelope').fadeOut(800, function() {
+          // Ocultar elementos dentro de .valentines-day
+          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+
+          // Hacer visible la carta con una animación ondulante
+          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+              var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+              $(this).css('transform', 'scale(' + scale + ')');
+          }}); // Animación de ondulación
+      });
+  });
+});
+
+ 
